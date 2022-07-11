8fafb3b246298ea04826e90730af2dd2dabb197f
Jack <108291349+Kupolad@users.noreply.github.com>
2022-07-11 17:36:04 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+console.log("test");
+
+
+
+new Twitch.Embed("video1", {
+    height: 300,
+    width: 350,
+    layout: "video",
+    channel: "xqc",
+    parent: "localhost",
+    muted: true
+});
+
+
+new Twitch.Embed("video2", {
+    height: 300,
+    width: 350,
+    layout: "video",
+    channel: "omie",
+    muted: true
+});
+
+new Twitch.Embed("video3", {
+    height: 300,
+    width: 350,
+    layout: "video",
+    channel: "omith",
+    muted: true
+});
+
+new Twitch.Embed("video4", {
+    height: 300,
+    width: 350,
+    layout: "video",
+    channel: "ripoozi",
+    muted: true
+});
+
+new Twitch.Embed("video5", {
+    height: 300,
+    width: 350,
+    layout: "video",
+    channel: "buddha",
+    muted: true
+});
